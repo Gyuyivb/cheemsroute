762c457b053bcf0a5f34212d2b958f6e06fecdc4
Isabela <isa31297@gmail.com>
2025-02-01 20:59:35 -0500

    add navlink and posts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
+import { Menu } from "./Menu";
 import { HomePage } from "./HomePage";
 import { BlogPage } from "./BlogPage";
+import { BlogPost } from "./BlogPost";
 import { ProfilePage } from "./ProfilePage";
-import { Menu } from "./Menu";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}/>
         <Route path="/blog" element={<BlogPage />}/>
+        <Route path="/blog/:slug" element={<BlogPost />}/>
         <Route path="/profile" element={<ProfilePage />}/>
         <Route path="*" element={<p>NOT FOUND</p>}/>
       </Routes>
---
+++ src/blogdata.js
@@ -0,0 +1,22 @@
+const blogdata = [];
+
+blogdata.push({
+    title:'Sobre Cheems',
+    slug: 'sobre-cheems',
+    content: 'Cheems es lindo y tiene amsieda.',
+    author: 'Gyuyi',
+});
+blogdata.push({
+    title:'La amsieda de cheems',
+    slug: 'la-amsieda-de-cheems',
+    content: 'El tiene amsieda a la hora de ordenar una anvorguesa.',
+    author: 'Gyuyi',
+});
+blogdata.push({
+    title:'La anvorguesa',
+    slug: 'la-anvorguesa',
+    content: 'Es deliciosa, pero la cajera asusta a cheems.',
+    author: 'Gyuyi',
+})
+
+export { blogdata };
